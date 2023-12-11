daa6e4572a06a7ccb454e4e343fa47f8caa14e3d
adityaapte <aditya.apte@gmail.com>
2023-12-11 12:34:22 -0500

    Setup to include all files in daatsets

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(
   name = "pycerr",
   packages = ["cerr", "cerr.dataclasses", "cerr.contour", "cerr.radiomics",
               "cerr.utils", "cerr.dcm_export", "cerr.datasets"],
+  package_data={
+    # Include all the files in "datasets"
+    "cerr.datasets": ["*"]
+	},       
   zip_safe = False
 )
