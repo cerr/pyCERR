312f6aeafe7bf4d5e54c23b517d4468d380dcc39
adityaapte <aditya.apte@gmail.com>
2023-12-11 12:44:02 -0500

    Set include_package_data to True

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
   name = "pycerr",
   packages = ["cerr", "cerr.dataclasses", "cerr.contour", "cerr.radiomics",
               "cerr.utils", "cerr.dcm_export", "cerr.datasets"],
+  include_package_data = True,
   package_data={
     # Include all the files in "datasets"
-    "cerr.datasets": ["*"]
+    "cerr.datasets": ["*dcm", "*.txt"]
 	},       
   zip_safe = False
 )
