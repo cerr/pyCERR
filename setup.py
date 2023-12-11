ea5ecf011dfebc80bfaf7e4498bb6ecec4d71199
adityaapte <aditya.apte@gmail.com>
2023-12-11 12:55:12 -0500

    aded radiomics_phantom_dicom

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
   packages = ["cerr", "cerr.dataclasses", "cerr.contour", "cerr.radiomics",
               "cerr.utils", "cerr.dcm_export", "cerr.datasets"],
   include_package_data = True,
-  package_data={
-    # Include all the files in "datasets"
-    "cerr.datasets": ["*dcm", "*.txt"]
-	},       
+  package_data = {'cerr.datasets': ['radiomics_phantom_dicom/PAT1/*.dcm',
+  			'radiomics_phantom_dicom/PAT2/*.dcm',
+  			'radiomics_phantom_dicom/PAT3/*.dcm',
+  			'radiomics_phantom_dicom/PAT4/*.dcm']},
   zip_safe = False
 )
