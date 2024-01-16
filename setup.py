61500f84e5158f2a271292de84e4e9b396b05de5
adityaapte <aditya.apte@gmail.com>
2024-01-16 10:22:19 -0500

    Added radiomics_settings/IBSIsettings/IBSI2Phase1/*.json

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
               "cerr.utils", "cerr.dcm_export", "cerr.datasets"],
   include_package_data = True,
   package_data = {'cerr.datasets': ['radiomics_settings/*.json',
+            'radiomics_settings/IBSIsettings/IBSI2Phase1/*.json',
             'radiomics_phantom_dicom/PAT1/*.dcm',
   			'radiomics_phantom_dicom/PAT2/*.dcm',
   			'radiomics_phantom_dicom/PAT3/*.dcm',
